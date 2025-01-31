dfd1a66a41b4a6e8a89bb5967303bd83188fc009
Shoaib <mdshoaib.alam22@gmail.com>
2025-01-31 14:21:25 +0530

    first changes

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const handleClick=()=>{
+    window.alert("clicked");
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello jenkins
+        </p>
+        <button onClick={handleClick}>Click me </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
